0708549df3c357368ac3e76129add84b5d62749a
Gregor Wolf <gregor.wolf@gmail.com>
2024-03-29 14:52:41 +0100

    WIP RFC connection

--- application/src/main/java/rfctest/models/HelloWorldResponse.java
+++ application/src/main/java/rfctest/models/HelloWorldResponse.java
@@ -3,12 +3,11 @@ package rfctest.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class HelloWorldResponse
-{
-    @JsonProperty("hello")
+public class HelloWorldResponse {
+    @JsonProperty("data")
     private final String name;
 
-    public HelloWorldResponse( final String name ) {
+    public HelloWorldResponse(final String name) {
         this.name = name;
     }
 }
